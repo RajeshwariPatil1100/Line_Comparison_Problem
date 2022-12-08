5fcbecc6d4409d48b7bdd98dc84633d3d6c86ac6
RajeshwariPatil1100 <rajeshwaripatil1100@gmail.com>
2022-12-08 17:19:52 +0530

    [Rajeshwari] Length Calculation

--- Line_Comparison_Problem/Program.cs
+++ Line_Comparison_Problem/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Line_Comparison_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+
+            int X1, X2, Y1, Y2;
+            Console.WriteLine("Enter co-ordinate value X1");
+            X1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter co-ordinate value Y1");
+            Y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter co-ordinate value X2 ");
+            X2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter co-ordinate value Y2");
+            Y2 = Convert.ToInt32(Console.ReadLine());
+            double length = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+            Console.WriteLine("The length of the Line is : " + length);
+        }
+    }
+}
